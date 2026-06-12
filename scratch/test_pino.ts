178077ec1c4f014992de8454478e1074dd62c482
Iqra Shamim <iqrashamim8546@gmail.com>
2026-06-12 23:05:03 +0530

    Remove generated JS files and disable emit in root tsconfig.json

--- scratch/test_pino.ts
+++
@@ -1,5 +0,0 @@
-import pinoHttp from "pino-http";
-import * as pinoHttpNamespace from "pino-http";
-
-console.log("Default export:", typeof pinoHttp, pinoHttp);
-console.log("Namespace keys:", Object.keys(pinoHttpNamespace));
